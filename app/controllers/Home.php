793eede67bc7f3e2c343fe56819d54f920fdfa91
Chamalfernando <chamaldeshitha2001@gmail.com>
2023-12-25 02:41:58 +0530

    completed the routing system

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,6 +8,20 @@ class Home
 {
     function __construct()
     {
-        echo "Home Page";
+        // echo "Home Page";
+    }
+
+    public function index($id){
+        echo "home view page".$id;
+    }
+
+    public function edit()
+    {
+        echo "home editing";
+    }
+
+    public function delete($id)
+    {
+        echo "home deleting ".$id;
     }
 }
