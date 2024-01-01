ed45ef2edf35f9f023eda5124fdd02cd19f31570
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-01-01 11:17:09 +0530

    commit for the work_19

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -6,8 +6,12 @@
 class Signup extends Controller
 {
     public function index(){
+        $user = new User();
+        $result = $user->validate($_POST);
         // $db = new Database();
         // $db->create_tables();
+        var_dump($result);
+        show($user->errors);
         show($_POST);
         $data['title'] = "Signup";
         $this->view('signup',$data);
